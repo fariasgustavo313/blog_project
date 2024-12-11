cefca260a21918444d5ebf215989d06ceafe9644
Farias Gustavo <fariasgustavo313@gmail.com>
2024-12-11 00:08:04 -0300

    Agregada la paginacion a la vista de posts

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from .models import Post
 
 def home(request):
-    posts = Post.objects.all().order_by("date_posted")
-    return render(request, "blog/home.html", {"posts": posts})
+    posts_list = Post.objects.all().order_by("date_posted") # Ordenamos por fecha de publicacion
+    paginator = Paginator(posts_list, 5) # 5 posts por pagina
+    page_number = request.GET.get("page") # Obtenemos el numero de pagina desde la URL
+    page_obj = paginator.get_page(page_number) # Obtener los posts de la pagina actual
+    return render(request, "blog/home.html", {"page_obj": page_obj})
